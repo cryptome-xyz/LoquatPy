159ab72eac955f8ab248822425c2a99dac8a9463
Xinyu Zhang <cryptome.xyz@gmail.com>
2024-02-06 13:00:59 +1100

    Verification is not finished yet.

--- loquat.py
+++ loquat.py
@@ -7,7 +7,7 @@ def main():
     sk, pk = loquat_keygen(pp)
     msg = "Hello World"
     sig = loquat_sign(pp, sk, msg)
-    result = loquat_verify(pp, pk, msg, sig)
+    # result = loquat_verify(pp, pk, msg, sig)
 
 if __name__ == '__main__':
     main()
